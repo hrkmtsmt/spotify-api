e2a50361ebe295f30562b0e3f58e113607910bd6
hrkmtsmt <hrkmtsmt@icloud.com>
2022-10-12 23:42:29 +0900

    :+1: [FEATURE]: Request Spotify API

--- src/env.ts
+++ src/env.ts
@@ -4,6 +4,7 @@ export const ENV = {
   CLIENT_ID: '29c328cd30844af5be36321621d5db9f',
   CLIENT_SECRET: '6cb1865f461a4ec6b09054e2f43bee73',
   REDIRECT_URI: 'http://localhost:8080/callback',
+  SPOTIFY_URL: 'https://api.spotify.com/v1',
   SPOTIFY_AUTH_URL: 'https://accounts.spotify.com/authorize',
   SPOTIFY_TOKEN_API_URL: 'https://accounts.spotify.com/api/token'
 } as const;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,65 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { useRecoilValue } from 'recoil';
+import axios from 'axios';
 import { AuthorizationButton } from '@src/components/RequestUserAuthorization';
 import { refreshTokenPostResponseState } from '@src/atoms/refreshTokenPostResponse';
+import { ENV } from '@src/env';
+
+const endpoints = [
+  {
+    id: 'albums',
+    name: 'Albums'
+  },
+  {
+    id: 'artists',
+    name: 'Artists'
+  },
+  {
+    id: 'tracks',
+    name: 'Tracks'
+  },
+  {
+    id: 'genres',
+    name: 'Genres'
+  }
+];
 
 export const Index: React.FC = () => {
   const refreshTokenPostResponse = useRecoilValue(
     refreshTokenPostResponseState
   );
+
+  const [someSpotifyId, setSomeSpotifyId] = useState('');
+
+  const onChangeSomeSpotifyId = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setSomeSpotifyId(e.target.value);
+    },
+    [someSpotifyId]
+  );
+
+  const [endpoint, setEndpoint] = useState<string>('albums');
+
+  const onChangeEndpoint = useCallback(
+    (e: React.ChangeEvent<HTMLSelectElement>) => {
+      setEndpoint(e.target.value);
+    },
+    [endpoint]
+  );
+
+  const [spotifyData, setSpotifyData] = useState<any>();
+
+  const onClickRequest = useCallback(async () => {
+    const { data } = await axios.get<any>(
+      `${ENV.SPOTIFY_URL}/${endpoint}/${someSpotifyId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${refreshTokenPostResponse.access_token}`
+        }
+      }
+    );
+    setSpotifyData(data);
+  }, [someSpotifyId, endpoint]);
 
   if (!refreshTokenPostResponse.access_token) {
     return (
@@ -18,7 +71,20 @@ export const Index: React.FC = () => {
 
   return (
     <div>
-      <button></button>
+      <input
+        type='text'
+        value={someSpotifyId}
+        onChange={onChangeSomeSpotifyId}
+      />
+      <select value={endpoint} onChange={onChangeEndpoint}>
+        {endpoints.map((item) => {
+          return <option value={item.id}>{item.name}</option>;
+        })}
+      </select>
+      <button onClick={onClickRequest}>Request</button>
+      <div>
+        {someSpotifyId} is {spotifyData ? spotifyData.name : '?'}
+      </div>
     </div>
   );
 };
